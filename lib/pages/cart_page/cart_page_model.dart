4ffae02774118e5bfc82606f5e9417f6bb653521
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2023-10-04 11:54:03 +0000

    added custom action and custom code

--- lib/pages/cart_page/cart_page_model.dart
+++ lib/pages/cart_page/cart_page_model.dart
@@ -1,6 +1,7 @@
 import '/flutter_flow/flutter_flow_theme.dart';
 import '/flutter_flow/flutter_flow_util.dart';
 import '/flutter_flow/flutter_flow_widgets.dart';
+import '/custom_code/actions/index.dart' as actions;
 import 'cart_page_widget.dart' show CartPageWidget;
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
@@ -10,6 +11,8 @@ class CartPageModel extends FlutterFlowModel<CartPageWidget> {
   ///  State fields for stateful widgets in this page.
 
   final unfocusNode = FocusNode();
+  // Stores action output result for [Custom Action - uuidGenerator] action in Button widget.
+  String? uuid;
 
   /// Initialization and disposal methods.
 
--- lib/pages/cart_page/cart_page_widget.dart
+++ lib/pages/cart_page/cart_page_widget.dart
@@ -1,6 +1,7 @@
 import '/flutter_flow/flutter_flow_theme.dart';
 import '/flutter_flow/flutter_flow_util.dart';
 import '/flutter_flow/flutter_flow_widgets.dart';
+import '/custom_code/actions/index.dart' as actions;
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:provider/provider.dart';
@@ -174,6 +175,45 @@ class _CartPageWidgetState extends State<CartPageWidget> {
                           Divider(
                             thickness: 1.0,
                             color: FlutterFlowTheme.of(context).alternate,
+                          ),
+                          Padding(
+                            padding: EdgeInsetsDirectional.fromSTEB(
+                                10.0, 10.0, 10.0, 10.0),
+                            child: Text(
+                              valueOrDefault<String>(
+                                _model.uuid,
+                                '324324-fsfasd88-fdf',
+                              ),
+                              style: FlutterFlowTheme.of(context).bodyMedium,
+                            ),
+                          ),
+                          FFButtonWidget(
+                            onPressed: () async {
+                              _model.uuid = await actions.uuidGenerator();
+
+                              setState(() {});
+                            },
+                            text: 'Generete UUID',
+                            options: FFButtonOptions(
+                              height: 40.0,
+                              padding: EdgeInsetsDirectional.fromSTEB(
+                                  24.0, 0.0, 24.0, 0.0),
+                              iconPadding: EdgeInsetsDirectional.fromSTEB(
+                                  0.0, 0.0, 0.0, 0.0),
+                              color: FlutterFlowTheme.of(context).primary,
+                              textStyle: FlutterFlowTheme.of(context)
+                                  .titleSmall
+                                  .override(
+                                    fontFamily: 'Readex Pro',
+                                    color: Colors.white,
+                                  ),
+                              elevation: 3.0,
+                              borderSide: BorderSide(
+                                color: Colors.transparent,
+                                width: 1.0,
+                              ),
+                              borderRadius: BorderRadius.circular(8.0),
+                            ),
                           ),
                         ],
                       ),
